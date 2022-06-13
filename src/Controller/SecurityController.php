19b972933a77f21af115179e256d25750266d6fe
YakouJohn <jyakou@gmx.fr>
2022-06-13 11:31:08 +0200

    13/06/2022

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/jyjyjyjyjy", name="app_login")
+     * @Route("/cccccccccc", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
